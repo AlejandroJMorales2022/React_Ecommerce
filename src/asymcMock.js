8ed34a95faddef56463b02140e7ee3c4016c39e5
ecos2021 <info@cabanasecosdelvalle.com.ar>
2023-04-28 11:01:55 -0300

    ecommerce_preentrega2_2

--- src/asymcMock.js
+++ src/asymcMock.js
@@ -6,7 +6,7 @@ const products = [
         name:'salamandra 14000kcal',
         price:14000,
         category: 'premium',
-        img:'../premium14000.png',
+        img:'../assets/images/premiun/premium14000.png',
         stock:12,
         descripcion: 'salamandra de alto rendimiento de 14kcal para calefaccionar hasta 70m2'
     },
@@ -15,7 +15,7 @@ const products = [
         name:'salamandra 18000kcal',
         price:32000,
         category: 'premium',
-        img:'./components/assets/images/premium/campana27000.png',
+        img:'../assets/images/premium/campana27000.png',
         stock:20,
         descripcion: 'salamandra de alto rendimiento de 18kcal para calefaccionar hasta 100m2'
     },
@@ -24,7 +24,7 @@ const products = [
         name:'salamandra 23000kcal',
         price:56000,
         category: 'premium',
-        img:'./assets/images/premiun/premium11000.png',
+        img:'../assets/images/premiun/premium11000.png',
         stock:8,
         descripcion: 'salamandra de alto rendimiento de 23kcla para calefaccionar hasta 140m2'
     }
